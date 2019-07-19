548be7339051eb24ce7bcfc0846647edf9275b5f
clarissa <clarissa.gasiciel@gmail.com>
2019-07-19 14:56:36 -0500

    Added unittest1 for testing Baron

---
+++ projects/gasiciec/dominion/unittest1.c
@@ -0,0 +1,33 @@
+/******************************************************************************
+* * PROGRAM NAME:   unittest1.c
+* * AUTHOR:         Clarissa Gasiciel
+* * DATE MODIFIED:  July 18, 2019
+* * DESCRIPTION:    Unit test for Baron card functionality
+******************************************************************************/
+#include "dominion.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TESTCARD "baron"
+
+
+int main()
+{
+  //Declare game struct
+  struct gameState game;
+
+  //Declare test game struct
+  struct gameState test;
+
+  //Seed random number generator
+  int seed = 1000;
+  //Set number of players
+  int numPlayers = 2;
+  //Initialize to first player
+  int thisPlayer = 0;
+
+
+
+  return 0;
+}
