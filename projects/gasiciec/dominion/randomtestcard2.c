3a92d6fe747ada63dd1c55a62b8e15910bbeeec2
clarissa <clarissa.gasiciel@gmail.com>
2019-07-24 23:45:43 -0500

    Added shells for random tests

---
+++ projects/gasiciec/dominion/randomtestcard2.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+* * PROGRAM NAME:   randomtestcard2.c
+* * AUTHOR:         Clarissa Gasiciel
+* * DATE MODIFIED:  July 27, 2019
+* * DESCRIPTION:    Random test for Minion card functionality
+******************************************************************************/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "rngs.h"
+#include "testHelper.h"
+
+#define TESTCARD "minion"
+
+
+int main()
+{
+
+  return 0;
+}
---
+++ projects/gasiciec/dominion/randomtestcard3.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+* * PROGRAM NAME:   randomtestcard3.c
+* * AUTHOR:         Clarissa Gasiciel
+* * DATE MODIFIED:  July 27, 2019
+* * DESCRIPTION:    Random test for Tribute card functionality
+******************************************************************************/
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "rngs.h"
+#include "testHelper.h"
+
+#define TESTCARD "tribute"
+
+
+int main()
+{
+
+  return 0;
+}
